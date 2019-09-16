64fb0809ee94b33d3d2be97ca9acfe56e043e120
EdHolbrow <88464@farnborough.ac.uk>
2019-09-16 21:16:18 +0100

    Added code to help build updating highscores

--- src/main/java/HighscoreController.java
+++ src/main/java/HighscoreController.java
@@ -59,10 +59,10 @@ public class HighscoreController {
             System.out.println("Database error: " + exception.getMessage());
         }
     }
-    public static void insertDatabase(int HighScoreID, String PlayerName, String Difficulty, int PositionOnBoard, int Score, int UserID) {
+    public static void insertDatabase(int HighScoreID, String PlayerName, String Difficulty, int PositionOnBoard, int Score, int UserID ) {
         try {
-            PreparedStatement ps = Main.db.prepareStatement("INSERT INTO HighScores (HighScoreID, PlayerName, Difficulty, PositionOnBoard, Score, UserID) VALUES (?, ?, ?, ?, ?, ?)");
-            ps.setInt(1, HighScoreID);
+            PreparedStatement ps = Main.db.prepareStatement("UPDATE HighScores SET PlayerName = ?, Difficulty = ?, PostitionOnBoard = ?, Score = ?, UserID = ?) WHERE HighScoreID = ?");
+
             ps.setString(2, PlayerName);
             ps.setString(3, Difficulty);
             ps.setInt(4, PositionOnBoard);
@@ -72,10 +72,9 @@ public class HighscoreController {
         } catch (Exception exception) {
             System.out.println("Database error: " + exception.getMessage());
         }
-
-
-
     }
+
+
     public static void updateDatabase( int topPosition ) {
         try {
             PreparedStatement ps = Main.db.prepareStatement("UPDATE HighScores SET PlayerName = ?, Difficulty = ?, Score = ?, UserID = ?) WHERE PositionOnBoard = ?");
@@ -101,9 +100,9 @@ public class HighscoreController {
     }
 
 
-    public static void findScoresBelow(int userscore) {
+    public static void findScoresBelow(int userscore, String userDifficulty) {
         try {
-            PreparedStatement ps = Main.db.prepareStatement("SELECT * FROM HighScores WHERE score >  userscore");
+            PreparedStatement ps = Main.db.prepareStatement("SELECT * FROM HighScores WHERE score >  userscore AND userDifficulty = Difficulty");
             ResultSet results = ps.executeQuery();
             int topPosition = 11;
             while (results.next()) {
@@ -117,17 +116,19 @@ public class HighscoreController {
                     topPosition = PositionOnBoard;
                 }
             }
-            selectToUpdate(topPosition);
+            int startpoint = 9;
+            selectToUpdate(topPosition, userDifficulty, startpoint);
 
         } catch (Exception exception) {
             System.out.println("Database error: " + exception.getMessage());
         }
     }
 
-    public static void selectToUpdate (int topPosition) {
+    public static void selectToUpdate (int topPosition, String userDifficulty, int startpoint) {
         try {
-            for(int i =9; i< topPosition; i++) {
-                PreparedStatement ps = Main.db.prepareStatement("SELECT * FROM HighScores WHERE score >  userscore");
+
+            while(startpoint < topPosition) {
+                PreparedStatement ps = Main.db.prepareStatement("SELECT * FROM HighScores WHERE PosistionOnBoard = startpoint AND userDifficulty = Difficulty");
                 ResultSet results = ps.executeQuery();
 
                 while (results.next()) {
@@ -137,21 +138,36 @@ public class HighscoreController {
                     int PositionOnBoard = results.getInt(4);
                     int Score = results.getInt(5);
                     int UserID = results.getInt(6);
+                    updateRowBelow(HighScoreID, PlayerName,Difficulty, PositionOnBoard, Score, UserID, userDifficulty, startpoint, topPosition );
+
                 }
 
             }
+
         } catch (Exception exception) {
             System.out.println("Database error: " + exception.getMessage());
         }
 
     }
 
+public static void updateRowBelow(int HighScoreID, String PlayerName, String Difficulty, int PositionOnBoard, int Score, int UserID, String userDifficulty, int startpoint, int topPosition){
+    try {
+        PreparedStatement ps = Main.db.prepareStatement("UPDATE HighScores SET PlayerName = ?, Score = ?, UserID = ?) WHERE HighScoreID = (Startpoint - 1)");
 
-
-
-
-
-
+        ps.setString(2, PlayerName);
+        ps.setInt(5, Score);
+        ps.setInt(6, UserID);
+        ps.executeUpdate();
+        startpoint = startpoint - 1;
+        if (startpoint < topPosition) {
+            selectToUpdate(topPosition, userDifficulty, startpoint);
+        }else{
+            insertDatabase(HighScoreID, PlayerName,Difficulty, PositionOnBoard, Score, UserID);
+        }
+    }catch (Exception exception) {
+        System.out.println("Database error: " + exception.getMessage());
+    }
+}
 
 
 

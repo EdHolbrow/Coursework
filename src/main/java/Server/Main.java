a68d11660317918290e2e1f9a3dd3a30d89cbab6
EdHolbrow <Edward.holbrow@gmail.com>
2019-11-04 15:14:07 +0000

    Added comments

--- src/main/java/Server/Main.java
+++ src/main/java/Server/Main.java
@@ -16,18 +16,18 @@ public class Main {
     public static Connection db = null;
 
     public static void main(String[] args) {
-
         //this opens the database
         openDatabase("CWDatabase1.db");
-
+        //this prepares the jersey servlet
         ResourceConfig config = new ResourceConfig();
         config.packages("Controllers");
         config.register(MultiPartFeature.class);
         ServletHolder servlet = new ServletHolder(new ServletContainer(config));
+        //this prepares the jetty server to listen to the specific port and connects the servlet to the server
         Server server = new Server(8081);
         ServletContextHandler context = new ServletContextHandler(server, "/");
         context.addServlet(servlet, "/*");
-
+        //this starts the server
         try {
             server.start();
             System.out.println("Server successfully started.");
@@ -38,15 +38,14 @@ public class Main {
         closeDatabase();
     }
 
-  //  HighscoreController.updateScore(11, "Greg", "Easy", 1);
+    //  HighscoreController.updateScore(11, "Greg", "Easy", 1);
 
 //HighscoreController.selectDifficulty("Easy");
-        //Controllers.HighscoreController.selectDatabase();
-        //Controllers.HighscoreController.insertDatabase();
-        // Controllers.HighscoreController.updateDatabase(PlayerName, Difficulty, PositionOnBoard, score, UserID);
-        // Controllers.UserController.deleteDatabase(5);
-        //this closes the database
-
+    //Controllers.HighscoreController.selectDatabase();
+    //Controllers.HighscoreController.insertDatabase();
+    // Controllers.HighscoreController.updateDatabase(PlayerName, Difficulty, PositionOnBoard, score, UserID);
+    // Controllers.UserController.deleteDatabase(5);
+    //this closes the database
 
 
     private static void openDatabase(String dbFile) {
